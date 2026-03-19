e9a22eee414208628b8f0eebfbaa5354759e0fca
kirankumar0714 <kirankumar.s@mitrahsoft.in>
2026-03-19 12:37:54 +0530

    Description of your changes

---
+++ string.js
@@ -0,0 +1,20 @@
+let string = "kiran"
+
+function capitalise(){
+    const  capital = string.toUpperCase();
+    console.log(capital)
+}
+function reverse(){
+    let rev = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        rev += string[i]
+    }
+    return rev;
+}
+function trim(){
+   const trimmed = string.trim();
+}
+
+capitalise();
+reverse();
+trim();
