f7cc1e66232bb106bc4e8df98cf110e6eb453e18
kirankumar0714 <kirankumar.s@mitrahsoft.in>
2026-03-25 10:43:42 +0530

    New commit message

--- math.js
+++ math.js
@@ -12,3 +12,22 @@ function divide (x,y){
 }
 console.log(add(3,4))
 console.log(subtract(3,4))
+
+
+function factorialIterative(n) {
+  if (n < 0) {
+    return "Factorial is not defined for negative numbers.";
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i; 
+  }
+  return result;
+}
+console.log(factorialIterative(5));
+console.log(factorialIterative(0)); 
+
