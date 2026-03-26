c4a2b66f6df5a92596f2a4f27d1ed224f99dae1d
kirankumar0714 <kirankumar.s@mitrahsoft.in>
2026-03-26 12:21:16 +0530

    cherry pick example

--- math.js
+++ math.js
@@ -1,14 +1,23 @@
-function add(x,y){
-    return x+y;
+
+function factorialIterative(n) {
+  if (n < 0) {
+    return "Factorial is not defined for negative numbers.";
+  }
+
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i; 
+  }
+  return result;
 }
-function subtract(x,y){
-    return x-y;
-}
-function multiply(x,y){
-    return x*y;
-}
-function divide (x,y){
-    return x/y;
-}
-console.log(add(3,4))
-console.log(subtract(3,4))
+console.log(factorialIterative(5));
+console.log(factorialIterative(0)); 
+console.log(factorialIterative(1)); 
+console.log(factorialIterative(2)); 
+console.log(factorialIterative(3)); 
+console.log(factorialIterative(4)); 
+console.log(factorialIterative(6)); 
+
